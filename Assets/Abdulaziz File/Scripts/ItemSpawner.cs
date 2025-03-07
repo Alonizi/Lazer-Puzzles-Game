2e7b1a43491ead5bce5ef152dff3c159d2ce5c68
Alonizi <Abdulaziz.Alonizi@gmail.com>
2025-03-07 23:10:09 +0300

    item exist check
    - added local function to check if a gameobject exist at the spawn location

--- Assets/Abdulaziz File/Scripts/ItemSpawner.cs
+++ Assets/Abdulaziz File/Scripts/ItemSpawner.cs
@@ -1,33 +1,45 @@
 using System;
 using Abdulaziz_File.Scripts;
+using UnityEditor.Tilemaps;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class ItemSpawner : MonoBehaviour
 {
+    [SerializeField]private MirrorRotator Mirror; 
+    [SerializeField]private LaserSplitter Splitter;
+    [SerializeField]private LaserSplitter Splitter_RGB;
+
+    
+    [SerializeField] private int Mirrors_MaxCount;
+    [SerializeField] private int Splitter_RGB_MaxCount;
+    [SerializeField] private int Splitter_MaxCount;
+    
     private Grid World;
-    private Mechanic? SelectedItem; 
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-
+    private Tilemap[] Tiles;
+    private Mechanic? SelectedItem;
+    
     private void Awake()
     {
         SelectedItem = null; 
         World = FindAnyObjectByType<Grid>();
     }
-
+    
     private void OnEnable()
     {
         Selector.OnItemSelected += SelectItem;
     }
-
+    
     /// <summary>
     /// check for user input , and spawn items accordingly
     /// </summary>
     void Update()
     {
+        
         if (Input.GetMouseButtonDown(0))
         {
-            if (SelectedItem is not null)
+            ItemExistInGrid(GetCurrentPositionToGrid());
+            if (SelectedItem is not null )
             {
                 SpawnItem();
             }
@@ -41,7 +53,19 @@ public class ItemSpawner : MonoBehaviour
     /// </summary>
     private void SpawnItem()
     {
-        
+        switch (SelectedItem)
+        {
+            case Mechanic.Mirror: 
+                break;
+            case Mechanic.Splitter:
+                break;
+            case Mechanic.Splitter_RGB:
+                break; 
+            case Mechanic.Rotator:
+                break; 
+            case Mechanic.Delete:
+                break; 
+        }
     }
     /// <summary>
     /// Get mouse position and convert it to Grid Position
@@ -63,9 +87,27 @@ public class ItemSpawner : MonoBehaviour
         Debug.LogWarning($"{item} Was Selected");
     }
 
+    /// <summary>
+    /// Check if a GameObject already exist at specified location
+    /// </summary>
+    /// <param name="gridPosition"> cell position </param>
+    /// <returns> true if item exit , false otherwise</returns>
+    private bool ItemExistInGrid(Vector3Int gridPosition)
+    {
+        bool itemExist = false; 
+        var tiles = FindObjectsByType<Tilemap>(FindObjectsSortMode.None);
+        foreach (Tilemap tile in tiles)
+        {
+            foreach (Transform item in tile.transform)
+            { 
+                itemExist = World.WorldToCell(item.position) == gridPosition ;
+            }
+        }
+        return itemExist;
+    }
+    
     private void OnDisable()
     {
         Selector.OnItemSelected -= SelectItem;
-
     }
 }
