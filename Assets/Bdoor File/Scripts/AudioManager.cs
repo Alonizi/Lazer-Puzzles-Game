b8bd0fbebeca9a7178e426b3dadd427b3ff2c00b
Bdoori <bdoor.alolayan@gmail.com>
2025-03-09 10:24:15 +0300

    Sfx vfx (#20)
    * Audio Manager & Settings
    
    * -

---
+++ Assets/Bdoor File/Scripts/AudioManager.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [Header("-------- Audio Source --------")]
+    [SerializeField] AudioSource Music;
+    [SerializeField] AudioSource SFX;
+
+    [Header("-------- Audio Clip --------")]
+
+    [Header("Background :")]
+    public AudioClip[] MusicClips;
+
+    [Header("SFX :")]
+    public AudioClip mirror;
+
+    public static AudioManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        PlayMusic(0);
+    }
+
+    public void PlayMusic(int index)
+    {
+        if (index >= 0 && index < MusicClips.Length)
+        {
+            Music.clip = MusicClips[index];
+            Music.Play();
+        }
+    }
+
+    public void PlaySFX(AudioClip clip)
+    {
+        SFX.PlayOneShot(clip);
+        //6:00
+    }
+
+}
