93ef3f616641551b98d40bc681792800530962bc
Alonizi <34661011+Alonizi@users.noreply.github.com>
2025-03-13 05:22:49 +0300

    Reactor polish part2 (#29)
    * adjust green laser material
    
    * Rotation and Light Adjustments
    
    Incrementally Rotate/Light .. up/down
    
    * adjust reactor script light setting
    
    -adjust reactor script light setting
    - adjust D&D test 1
    
    * adjust light flicker
    
    * sounds
    
    added sounds
    fix issue with one color receiver
    convert lvl2 to D&D
    adjust some D&D lvls for new prefabs
    
    * adjust d&d lvl test 2
    
    * new sprites
    
    added all assets with new sprites to beta assets tile map
    
    * change splitter default color
    
    * change splitter color on hit
    
    * added floor

--- Assets/Abdulrhman File/Scripts/ColorReceiver.cs
+++ Assets/Abdulrhman File/Scripts/ColorReceiver.cs
@@ -4,6 +4,7 @@ using UnityEditor;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
 using UnityEngine.SceneManagement;
+using Random = UnityEngine.Random;
 
 
 public class SimpleColorReceiver : MonoBehaviour
@@ -48,10 +49,24 @@ public class SimpleColorReceiver : MonoBehaviour
     public Light2D[] DiagonalLights;
     public Light2D CenterLight;
 
-    private Rigidbody2D[] ReactorRigids; 
+    private Rigidbody2D[] ReactorRigids;
+
+    private float ReactorSpinSpeed = 0f;
+    private float InitialCenterLightIntensity;
+    private float InitialDiagonalLightIntensity;
+    private float LightTimer = 0;
+    
+    [SerializeField] private AudioSource Reactor_ON;
+    [SerializeField] private AudioSource Reactor_OFF;
+    [SerializeField] private AudioSource Reactor_Running;
+    private int OnCounter= 0;
+    private int OffCounter =0;
     
     private void Awake()
     {
+        //Reactor_Running.loop = true; 
+        InitialCenterLightIntensity = CenterLight.intensity;
+        InitialDiagonalLightIntensity = DiagonalLights[0].intensity;
         spriteRenderer = GetComponent<SpriteRenderer>();
         if (spriteRenderer == null)
         {
@@ -160,16 +175,16 @@ public class SimpleColorReceiver : MonoBehaviour
         
         if (two)
         {
+            PlayAudio(color1Reached && color2Reached);
             RotateBase(color1Reached, color2Reached);
             ActivateEffects(color1Reached, color2Reached);
         }
         else
         {
-            RotateBase(color1Reached, true);
-            ActivateEffects(color1Reached, true);
-
+            PlayAudio(color1Reached);
+            RotateBase(color1Reached, color1Reached);
+            ActivateEffects(color1Reached, color1Reached);
         }
-
         laserColors.Clear(); // Reset for the next frame
     }
 
@@ -248,16 +263,106 @@ public class SimpleColorReceiver : MonoBehaviour
 
     private void ActivateEffects(bool color1 , bool color2)
     {
-        CenterLight.enabled = color1 && color2 ;
+        //CenterLight.enabled = color1 && color2 ;
+        CenterLight.enabled = true;
+        if (color1 && color2)
+        {
+            if (CenterLight.intensity < InitialCenterLightIntensity)
+            {
+                CenterLight.intensity += .01f;
+            }
+        }
+        else
+        {
+            if (CenterLight.intensity > 0)
+            {
+                CenterLight.intensity -= .015f;
+            }
+        }
+
+        var freq =  Random.Range(.25f, 1f);
         foreach (var light in DiagonalLights)
         {
-            light.enabled = color1 || color2 ;
+            light.enabled = true;
+            //light.enabled = color1 || color2 ;
+            if (color1 && color2)
+            {
+                if (light.intensity < InitialDiagonalLightIntensity)
+                {
+                    light.intensity += .01f;
+                }
+                
+            }
+            else if (color1 || color2)
+            {
+                lightstrobe(light,freq);
+            }
+            else
+            {
+                if (light.intensity > 0)
+                {
+                    light.intensity -= .01f;
+                }
+            }
         }
     }
 
     private void RotateBase( bool color1 , bool color2)
-    {
+    { 
         //ReactorRigids[0].rotation += 60f * Time.deltaTime * (color1 || color2 ? 1:0);
-        ReactorRigids[1].rotation -= 35f * Time.deltaTime * (color1 && color2 ? 1:0); 
+        // ReactorRigids[1].rotation -= ReactorSpinSpeed * Time.deltaTime * (color1 && color2 ? 1:0);
+       
+        if( color1 && color2)
+        {
+            if (ReactorSpinSpeed < 250f)
+            {
+                ReactorSpinSpeed += .5f;
+            }
+        }
+        else
+        {
+            if (ReactorSpinSpeed > 0)
+            {
+                ReactorSpinSpeed -= 0.5f;
+            }
+        }
+        ReactorRigids[1].rotation -= ReactorSpinSpeed * Time.deltaTime;
     }
+
+    private void lightstrobe(Light2D light , float freq)
+    {
+        LightTimer += Time.deltaTime;
+        if (LightTimer > freq)
+        {
+            light.intensity = Random.Range(0f, 3f);
+            LightTimer = 0; 
+        }
+    }
+
+    private void PlayAudio(bool on )
+    {
+        if (on && OnCounter <1)
+        {
+            Reactor_Running.Stop();
+            Reactor_OFF.Stop();
+            Reactor_ON.Play();
+            OnCounter++;
+            OffCounter = 0; 
+        }
+        if (on && !Reactor_ON.isPlaying && !Reactor_Running.isPlaying)
+        {
+            Reactor_Running.Play();
+        }
+        if (!on && OffCounter<1 )
+        {
+            OffCounter++;
+            OnCounter = 0; 
+            Reactor_ON.Stop();
+            Reactor_Running.Stop();
+            Reactor_OFF.Play();
+        }
+
+
+    }
+    
 }
