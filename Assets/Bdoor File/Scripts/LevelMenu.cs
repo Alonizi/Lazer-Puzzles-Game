64dac640d0aec7595243a8c69302d64be6cc35fa
Bdoori <bdoor.alolayan@gmail.com>
2025-03-19 10:27:18 +0300

    polish +++

--- Assets/Bdoor File/Scripts/LevelMenu.cs
+++ Assets/Bdoor File/Scripts/LevelMenu.cs
@@ -13,15 +13,23 @@ public class LevelMenu : MonoBehaviour
 
     private void Awake()
     {
-        int unlockedLevel = PlayerPrefs.GetInt("UnlockedLevel", 1);
+        int unlockedLevel = PlayerPrefs.GetInt("UnlockedLevel", 2);
+
         for (int i = 0; i < buttons.Length; i++)
         {
-            buttons[i].interactable = false;
-            lockImages[i].gameObject.SetActive(true);
+            buttons[i].interactable = false; 
+            lockImages[i].gameObject.SetActive(true); 
         }
-        for (int i = 0; i < unlockedLevel; i++)
+
+        buttons[0].interactable = true;
+        lockImages[0].gameObject.SetActive(false);
+
+        buttons[1].interactable = true;
+        lockImages[1].gameObject.SetActive(false);
+
+        for (int i = 2; i < unlockedLevel; i++)
         {
-            buttons[i].interactable |= true;
+            buttons[i].interactable = true;
             lockImages[i].gameObject.SetActive(false);
         }
     }
@@ -44,5 +52,17 @@ public class LevelMenu : MonoBehaviour
 
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
+
+    public void CompleteLevel(int currentLevel)
+    {
+        int unlockedLevel = PlayerPrefs.GetInt("UnlockedLevel", 2);
+
+        if (currentLevel + 1 > unlockedLevel)
+        {
+            PlayerPrefs.SetInt("UnlockedLevel", currentLevel + 1);
+            PlayerPrefs.Save();
+        }
+    }
+
 
 }
