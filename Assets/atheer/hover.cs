5b8891a4018bc8c15520946f3338039153a1b877
Aythai <cute_rooty@hotmail.com>
2025-03-20 08:57:52 +0300

    .

--- Assets/atheer/hover.cs
+++ Assets/atheer/hover.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class ButtonHoverEffect : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
@@ -7,22 +8,36 @@ public class ButtonHoverEffect : MonoBehaviour, IPointerEnterHandler, IPointerEx
     private Vector3 hoverScale;
     private float scaleSpeed = 5f; // Adjust speed as needed
     private bool isHovered = false;
+    private Button button; // Reference to the Button component
 
     void Start()
     {
         defaultScale = transform.localScale;
-        hoverScale = defaultScale * 1.2f; // Increase by 20%
+        hoverScale = defaultScale * 1.2f; // Scale up by 20%
+        button = GetComponent<Button>(); // Get the Button component
     }
 
     void Update()
     {
-        // Smooth transition
-        transform.localScale = Vector3.Lerp(transform.localScale, isHovered ? hoverScale : defaultScale, Time.deltaTime * scaleSpeed);
+        // Smooth transition only if the button is interactable
+        if (button != null && button.interactable)
+        {
+            transform.localScale = Vector3.Lerp(transform.localScale, isHovered ? hoverScale : defaultScale, Time.deltaTime * scaleSpeed);
+        }
+        else
+        {
+            // Reset scale if button becomes non-interactable
+            transform.localScale = defaultScale;
+        }
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        isHovered = true;
+        // Only trigger hover effect if button is interactable
+        if (button != null && button.interactable)
+        {
+            isHovered = true;
+        }
     }
 
     public void OnPointerExit(PointerEventData eventData)
